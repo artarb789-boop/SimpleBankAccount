2c60b3761fac02d4b8077dfdef4b3ef37135d541
Artem Kazakov <artarb789@gmail.com>
2025-10-03 21:22:08 +0200

    BankAccount complete

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,6 +2,7 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BankAccount {
 
@@ -22,6 +23,47 @@ public class BankAccount {
         transactionHistory.add(LocalDateTime.now() + ": " + operation + " | Баланс: " + balance);
     }
 
+    private void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Вносимая сумма должна быть положительной");
+        }
+        this.balance += amount;
+        addToHistory("Пополнение: " + amount);
+
+    }
+
+    private void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Снимаемая сумма должна быть положительной");
+        }
+        if(amount > balance){
+            throw new InsufficientFundsException("На балансе не достаточно средств");
+        }
+        this.balance -= amount;
+        addToHistory("Снятие: " + amount);
+
+    }
+
+    private void transfer(int idSource, int idDestination, double amount) {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof BankAccount that)) return false;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Счёт #%d | Владелец: %s | Баланс: %.2f", id, owner, balance);
+    }
+
     public int getId() {
         return id;
     }
@@ -36,10 +78,6 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
     }
 
 }
